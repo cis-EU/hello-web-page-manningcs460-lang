4818f292a2ee959b0cca6778ef93e751ef00611d
manningcs460-lang <manningcs460@emmaus.edu>
2026-03-17 13:54:23 -0500

    Create mathmodule.js
    exporting just the add() function

---
+++ mathmodule.js
@@ -0,0 +1,7 @@
+// modules example from w3schools
+// learning about modules
+
+// Export an "add" function
+export function add(a, b) {  // export for other javascript code to use!
+  return a + b;
+}
